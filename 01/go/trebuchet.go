64a619178c01f345a9af183d71adafd33b7a2248
8moonx@gmail.com <8moonx@gmail.com>
2024-02-11 17:27:32 +0100

    compelted part 2

--- 01/go/trebuchet.go
+++ 01/go/trebuchet.go
@@ -4,14 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
-	"text/scanner"
 	"unicode"
 )
 
 func main() {
-	//fmt.Println("Hello World!")
 	part1()
 	part2()
 }
@@ -21,54 +20,51 @@ func part1() {
 	var line_number strings.Builder
 
 	file, error := os.Open("example_part1.txt")
-//	file, error := os.Open("input_part1.txt")
+	//	file, error := os.Open("input_part1.txt")
 	if error != nil {
 		fmt.Println("Error opening file:", error)
 		return
 	}
 	defer file.Close()
-	
+
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		for i:=0; i < len(line); i++ {
+		for i := 0; i < len(line); i++ {
 			if unicode.IsDigit(rune(line[i])) {
-			line_number.WriteRune(rune(line[i]))
-			break
+				line_number.WriteRune(rune(line[i]))
+				break
 			}
 		}
-		for i:=len(line)-1; i > 0; i-- {
+		for i := len(line) - 1; i > 0; i-- {
 			if unicode.IsDigit(rune(line[i])) {
-			line_number.WriteRune(rune(line[i]))
-			break
+				line_number.WriteRune(rune(line[i]))
+				break
 			}
 		}
 		number, _ := strconv.Atoi(line_number.String())
-//		fmt.Println("string number: " + line_number.String())
 		line_number.Reset()
 		total = total + number
-//		fmt.Printf("total: %v\n", total)
 	}
-//	fmt.Println("string number: " + line_number.String())
 	fmt.Printf("total: %v\n", total)
 }
 
 func part2() {
-	// Create Map as dictionary to map int numbers to string numbers
-	dictionnary := map[int]string {
-		1:"one",
-		2:"two",
-		3:"three",
-		4:"four",
-		5:"five",
-		6:"six",
-		7:"seven",
-		8:"eight",
-		9:"nine",
+	dictionnary := map[string]string{
+		"1": "one",
+		"2": "two",
+		"3": "three",
+		"4": "four",
+		"5": "five",
+		"6": "six",
+		"7": "seven",
+		"8": "eight",
+		"9": "nine",
 	}
+	total := 0
 	file, error := os.Open("example_part2.txt")
-//	file, error := os.Open("input_part2.txt")
+	//file, error := os.Open("input_part2.txt")
 	if error != nil {
 		fmt.Println("Error opening file:", error)
 		return
@@ -78,21 +74,31 @@ func part2() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		// Find first and last number from each line and add it to total sum
 		line := scanner.Text()
-		// Create Map to find first string number in line
 		index := make(map[int]string)
-		// run through dictionnary to find place of each word in line
 		for key := range dictionnary {
 			index[strings.Index(line, dictionnary[key])] = dictionnary[key]
+			index[strings.Index(line, key)] = key
 		}
-		// delete not found
 		delete(index, -1)
-		// strings.replace string number with int number
-		// find first and last number in string 
-		// build number and add it to total sum
+		index_keys := make([]int, 0, len(index))
+		for key := range index {
+			index_keys = append(index_keys, key)
+		}
+		sort.Ints(index_keys)
+		smallest_index_key := index_keys[0]
+		largest_index_key := index_keys[len(index_keys)-1]
+		line_result := index[smallest_index_key] + index[largest_index_key]
+		line_result_int := line_result
+		for key, value := range dictionnary {
+			line_result_int = strings.ReplaceAll(line_result_int, value, key)
+		}
+		line_int, _ := strconv.Atoi(line_result_int)
+		total += line_int
 	}
 	if error := scanner.Err(); error != nil {
 		fmt.Println("Error reading file:", error)
 	}
+	fmt.Printf("total: %v\n", total)
+
 }
