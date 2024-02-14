2664c7595990be0a035bd276636be19cbbd7ad85
8moonx@gmail.com <8moonx@gmail.com>
2024-02-14 21:32:19 +0100

    finished part 2

--- 02/java/CubeConundrum.java
+++ 02/java/CubeConundrum.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -57,54 +58,41 @@ public class CubeConundrum {
 
     private static void part2(){
         List<String> input = new ArrayList<>();
-        try (BufferedReader br = Files.newBufferedReader(Paths.get("02\\java\\example2.txt"))) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("02\\java\\input2.txt"))) {
             input = br.lines().collect(Collectors.toList());
         } catch (IOException exception) {
             exception.printStackTrace();
         }
         int total = 0;
         for (String line : input) {
-            List<String> valdiationList = new ArrayList<>();
-            String gameId = line.substring(5, line.indexOf(":")).trim();
+            HashMap<String, Integer> setofCubes = new HashMap<>();
+            setofCubes.put("red", 0);
+            setofCubes.put("green", 0);
+            setofCubes.put("blue", 0);
             String reveals = line.substring(line.indexOf(":") + 1).trim();
             String[] reveal = reveals.split(";");
             for (String revealString : reveal) {
                 String[] colours = revealString.split(",");
                 for (String colour : colours) {
-                    // Instead of valid and invalid game check find the highest amount of each colour
-/*
-Create a Map with colour:amount
-starting amount = 0
-check amount in map with amount from current ; substring
-if substring amount > map amount -> write substring amount into map 
-*/ 
-                    valdiationList.add("valid");
                     if (colour.contains("red")) {                        
                         String redAmount = colour.replace("red", "").trim();
-                        if (Integer.parseInt(redAmount) > 12) {
-                            valdiationList.add("invalid");
+                        if (Integer.parseInt(redAmount) > setofCubes.get("red")) {
+                            setofCubes.put("red", Integer.parseInt(redAmount));
                         }
                     } else if (colour.contains("green")) {
                         String greenAmount = colour.replace("green", "").trim();
-                        if (Integer.parseInt(greenAmount) > 13) {
-                            valdiationList.add("invalid");
+                        if (Integer.parseInt(greenAmount) > setofCubes.get("green")) {
+                            setofCubes.put("green", Integer.parseInt(greenAmount));
                         }
                     } else if (colour.contains("blue")) {
                         String blueAmount = colour.replace("blue", "").trim();
-                        if (Integer.parseInt(blueAmount) > 14) {
-                            valdiationList.add("invalid");
+                        if (Integer.parseInt(blueAmount) > setofCubes.get("blue")) {
+                            setofCubes.put("blue", Integer.parseInt(blueAmount));
                         }
                     }
                 }
             }
-            // Multiply the cube amounts with each other <-> game id
-/*
- * read colour amounts out of map and multiply them
- */
-            // sum the multiplication results instead of game id
-            if (valdiationList.contains("invalid") == true) {} else {
-                total += Integer.parseInt(gameId);
-            }
+            total += setofCubes.get("red") * setofCubes.get("green") * setofCubes.get("blue");
         }
         System.out.println(total);
     }
