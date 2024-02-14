5c8c24673bc839b64845f3567c4a42a9cc63c1cd
8moonx@gmail.com <8moonx@gmail.com>
2024-02-14 21:23:36 +0100

    added example file, added input file, added thouhts for solution

--- 02/java/CubeConundrum.java
+++ 02/java/CubeConundrum.java
@@ -10,6 +10,7 @@ public class CubeConundrum {
 
     public static void main(String[] args) {
         part1();
+        part2();
     }
 
     private static void part1(){
@@ -47,6 +48,60 @@ public class CubeConundrum {
                     }
                 }
             }
+            if (valdiationList.contains("invalid") == true) {} else {
+                total += Integer.parseInt(gameId);
+            }
+        }
+        System.out.println(total);
+    }
+
+    private static void part2(){
+        List<String> input = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("02\\java\\example2.txt"))) {
+            input = br.lines().collect(Collectors.toList());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        int total = 0;
+        for (String line : input) {
+            List<String> valdiationList = new ArrayList<>();
+            String gameId = line.substring(5, line.indexOf(":")).trim();
+            String reveals = line.substring(line.indexOf(":") + 1).trim();
+            String[] reveal = reveals.split(";");
+            for (String revealString : reveal) {
+                String[] colours = revealString.split(",");
+                for (String colour : colours) {
+                    // Instead of valid and invalid game check find the highest amount of each colour
+/*
+Create a Map with colour:amount
+starting amount = 0
+check amount in map with amount from current ; substring
+if substring amount > map amount -> write substring amount into map 
+*/ 
+                    valdiationList.add("valid");
+                    if (colour.contains("red")) {                        
+                        String redAmount = colour.replace("red", "").trim();
+                        if (Integer.parseInt(redAmount) > 12) {
+                            valdiationList.add("invalid");
+                        }
+                    } else if (colour.contains("green")) {
+                        String greenAmount = colour.replace("green", "").trim();
+                        if (Integer.parseInt(greenAmount) > 13) {
+                            valdiationList.add("invalid");
+                        }
+                    } else if (colour.contains("blue")) {
+                        String blueAmount = colour.replace("blue", "").trim();
+                        if (Integer.parseInt(blueAmount) > 14) {
+                            valdiationList.add("invalid");
+                        }
+                    }
+                }
+            }
+            // Multiply the cube amounts with each other <-> game id
+/*
+ * read colour amounts out of map and multiply them
+ */
+            // sum the multiplication results instead of game id
             if (valdiationList.contains("invalid") == true) {} else {
                 total += Integer.parseInt(gameId);
             }
